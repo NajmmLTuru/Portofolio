afbe7c01b00d8af7d46023e45204bd3f72c0ff6e
NajmmLTuru <mnfsantri@gmail.com>
2026-05-26 16:38:37 +0700

    3

---
+++ src/config/index.ts
@@ -0,0 +1,97 @@
+import type { SiteConfig, SiteContent } from "../types";
+
+export const SITE_CONFIG: SiteConfig = {
+  title: "Muhammad Najmul Fahmi - Biodata & Portofolio",
+  author: "M. Najmul Fahmi",
+  description:
+    "Building digital experiences through data, design, and code.",
+  lang: "en",
+  siteLogo: "/najmm-small.jpg",
+  navLinks: [
+    { text: "Experience", href: "#experience" },
+    { text: "Projects", href: "#projects" },
+    { text: "About", href: "#about" },
+  ],
+  socialLinks: [
+    { text: "Twitter", href: "https://x.com/Najmmlturu" },
+    { text: "LinkedIn", href: "https://www.linkedin.com/in/muhammad-najmul-fahmi-58704236b" },
+    { text: "Github", href: "https://github.com/NajmmLTuru" },
+    { text: "Youtube", href: "https://youtube.com/@lord_of_turu?si=IoTm8EHiWxB9g4k9" },
+    { text: "Instagram", href: "https://www.instagram.com/lord_of_turu/" },
+  ],
+  socialImage: "/zen-og.png",
+  canonicalURL: "https://astro-zen.vercel.app",
+};
+
+export const SITE_CONTENT: SiteContent = {
+  hero: {
+    name: "M. Najmul Fahmi",
+    specialty: "Data Analyst, App Developer, & Graphics Designer",
+    summary:
+      "Building digital experiences through data, design, and code.",
+    email: "mnfsantri@gmail.com",
+  },
+  experience: [
+    {
+      company: "",
+      position: "",
+      startDate: "",
+      endDate: "",
+      summary: [
+        "",
+        "",
+        "",
+      ],
+    },
+    {
+      company: "",
+      position: "",
+      startDate: "",
+      endDate: "",
+      summary: [
+        "",
+        "",
+      ],
+    },
+    {
+      company: "",
+      position: "",
+      startDate: "",
+      endDate: "",
+      summary:
+        "",
+    },
+  ],
+  projects: [
+    {
+      name: "",
+      summary: "",
+      linkPreview: "",
+      linkSource: "",
+      image: "/",
+    },
+    {
+      name: "",
+      summary: "",
+      linkPreview: "",
+      linkSource: "",
+      image: "/",
+    },
+    {
+      name: "",
+      summary: "",
+      linkPreview: "",
+      linkSource: "",
+      image: "/",
+    },
+  ],
+  about: {
+    description: `
+      Self-taught learner focused on data analysis, app development, and graphic design.
+      Passionate about building digital experiences through technology, creativity, and continuous growth.
+    `,
+    image: "/Najmm-big.jpg",
+  },
+};
+
+// #5755ff
